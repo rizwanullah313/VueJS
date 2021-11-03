e22eb2faa09454db31db89bedee29ba722fb9bcb
rizwanullah313 <ru323091@gmail.com>
2021-11-03 19:06:51 +0000

    increment and decrement with different methods and parametors

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+
+
+new Vue({
+   el:'#vue-app',
+
+ });
+
+
+
+// Code 2
+// new Vue({
+//    el:'#vue-app',
+//        data:{
+//            age: 45,
+//            },
+//            methods:{
+//              addage: function(inc){
+//                 this.age += inc;
+//              },
+//              remage: function(dec){
+//                 this.age -= dec;
+//              },
+//              aget: function(event){
+//                 this.age += event;
+//              },
+//              agem: function(event){
+//                 this.age -= event;
+//              }
+//            }
+//  });
+
+
+
+
+
+
+// Code 1
 // new Vue({
 //     el:'#vue-app',
 //     data:{
