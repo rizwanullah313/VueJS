8024996d2570a854f01208a76158de1f796ed7ef
rizwanullah313 <ru323091@gmail.com>
2021-11-03 18:41:44 +0000

    Add method and link websites

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-new Vue({
-    el:'#vue-app',
-    data:{
-      name: "RizwanUllah",
-      website: "https://github.com/rizwanullah313?tab=repositories",
-    }
-
-});
+// new Vue({
+//     el:'#vue-app',
+//     data:{
+//       name: "RizwanUllah",
+//       website: "https://github.com/rizwanullah313?tab=repositories",
+//       websiteTag: '<a href="https://atom.io/packages/atom-live-server">Atom Live Server</a>'
+//     },
+//     methods: {
+//       greet: function(time){
+//         return "Good "+time;
+//       }
+//     }
+//
+// });
