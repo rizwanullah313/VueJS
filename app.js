99226227a494b17bf829ef6d51ac647de876d67a
rizwanullah313 <ru323091@gmail.com>
2021-11-03 19:23:26 +0000

    simple punh game

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 
 
-new Vue({
-   el:'#vue-app',
-    data:{
 
-    },
-    methods: {
 
-    }
- });
 
+// code 4
+// new Vue({
+//     el:'#vue-app',
+//    data:{
+//      health: 100,
+//      ended: false
+//    },
+//    methods:{
+//      punch: function(){
+//        this.health -= 10;
+//        if(this.health <= 0){
+//          this.ended = true;
+//        }
+//      },
+//      restart: function(){
+//        this.health = 100;
+//        this.ended = false;
+//      }
+//    },
+//    computed: {
+//
+//    }
+// });
 
 
 // Code 3
