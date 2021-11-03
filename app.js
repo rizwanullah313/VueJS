db366b171821c2e36b55fd270e6c23c3b0547c53
rizwanullah313 <ru323091@gmail.com>
2021-11-03 20:07:39 +0000

    done with basic Vue js

--- app.js
+++ app.js
@@ -1,5 +1,100 @@
 
 
+//  code 8 computed distributed
+// new Vue({
+//   el:'#vue-app',
+//    data: {
+//      age: 20,
+//      a: 0,
+//      b: 0
+//    },
+//
+//    methods: {
+//      // AddToA: function(){
+//      //   console.log("AddToA");
+//      //   return this.a + this.age;
+//      // },
+//      // AddToB: function(){
+//      //   console.log("AddToB");
+//      //   return this.b + this.age ;
+//      // },
+//      // before computed property run both if click once result in console.log();
+//    },
+//  computed:{
+//    AddToA: function(){
+//      console.log("AddToA");
+//      return this.a + this.age;
+//    },
+//    AddToB: function(){
+//      console.log("AddToB");
+//      return this.b + this.age ;
+//    },
+//  }
+// });
+
+
+
+// code 7
+// Vue.component('greeting',{
+//   template: '<p> Hey there, I am a re-usable {{name}}.<button v-on:click="changeName">Change Name</button></p>',
+//   data: function(){
+//     return {
+//       name: 'Karachi'
+//     }
+//   },
+//   methods: {
+//     changeName: function(){
+//       this.name = 'Mario';
+//     }
+//   }
+// });
+//
+// new Vue({
+//   el: '#vue-app-one'
+// });
+//
+// new Vue({
+//   el: '#vue-app-two'
+// });
+
+
+
+
+// code 6
+// var one = new Vue({
+//   el:'#vue-app-one',
+//   data:{
+//      title: 'Vue App One'
+//   },
+//   methods:{
+//
+//   },
+//   computed:{
+//      greet: function(){
+//        return 'Hello from app one:)';
+//      }
+//   }
+// });
+//
+// var two = new Vue({
+//   el:'#vue-app-two',
+//   data:{
+//      title: 'Vue App two'
+//   },
+//   methods:{
+//      changeTitle: function(){
+//        one.title = "Title changed"
+//      }
+//   },
+//   computed:{
+//     greet: function(){
+//       return 'Yo dudes, this is app 2 speaking to ya:)';
+//     }
+//   }
+// });
+// two.title = "Change from Outside";
+//
+//
 
 
 // code 5
